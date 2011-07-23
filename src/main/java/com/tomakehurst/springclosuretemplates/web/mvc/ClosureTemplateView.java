3e435172ded3b0dd7d180d841974bed40760138f
Tom Akehurst <t.m.akehurst@googlemail.com>
2011-07-23 11:44:20 +0100

    Initial project creation

---
+++ src/main/java/com/tomakehurst/springclosuretemplates/web/mvc/ClosureTemplateView.java
@@ -0,0 +1,20 @@
+package com.tomakehurst.springclosuretemplates.web.mvc;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.view.AbstractTemplateView;
+
+public class ClosureTemplateView extends AbstractTemplateView {
+
+	@Override
+	protected void renderMergedTemplateModel(Map<String, Object> model,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/tomakehurst/springclosuretemplates/web/mvc/ClosureTemplateViewResolver.java
@@ -0,0 +1,7 @@
+package com.tomakehurst.springclosuretemplates.web.mvc;
+
+import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
+
+public class ClosureTemplateViewResolver extends AbstractTemplateViewResolver {
+
+}
