1cac9252d1751feeed2708657dc47f84114cd210
zonski <L3tmeinn*w>
2012-08-11 18:23:48 +1000

    - Added recursive directory searching within the soy template directory to allow for organising soy files into sub-directories.
    - Set servlet-api scope 'provided' in the pom so it is not passed through to final build (this is provided by servlet engines, such as Tomcat).
    
    - Fixed a minor typo in pom for the Eclipse plugin (downloadSources was misspelled).

--- spring-closure-templates/src/main/java/com/tomakehurst/springclosuretemplates/web/mvc/ClosureTemplateViewResolver.java
+++ spring-closure-templates/src/main/java/com/tomakehurst/springclosuretemplates/web/mvc/ClosureTemplateViewResolver.java
@@ -1,60 +1,60 @@
 package com.tomakehurst.springclosuretemplates.web.mvc;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-
+import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.tofu.SoyTofu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
 
-import com.google.template.soy.SoyFileSet;
-import com.google.template.soy.tofu.SoyTofu;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClosureTemplateViewResolver extends AbstractTemplateViewResolver {
 
 	@Autowired
 	private ClosureTemplateConfig closureTemplateConfig;
-	
+
 	private SoyTofu compiledTemplates;
-	
+
 	public ClosureTemplateViewResolver() {
 		super();
-		setExposeSpringMacroHelpers(false);
+        setExposeSpringMacroHelpers(false);
 	}
-	
+
 	@Override
 	protected void initApplicationContext() {
 		super.initApplicationContext();
-		
+
 		if (isCache()) {
 			compiledTemplates = compileTemplates();
 		}
 	}
-	
-	private SoyTofu compileTemplates() {
-		File[] templateFiles;
-		Resource templatesLocation = closureTemplateConfig.getTemplatesLocation();
-		try {
-			templateFiles = templatesLocation.getFile().listFiles(new FilenameFilter() {
 
-				@Override
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".soy");
-				}
-			});
-		} catch (IOException ioe) {
-			throw new IllegalArgumentException(templatesLocation.toString() + " does not exist or is not a directory", ioe);
-		}
-		
-		SoyFileSet.Builder fileSetBuilder = new SoyFileSet.Builder();
-		for (File templateFile: templateFiles) {
-			fileSetBuilder.add(templateFile);
-		}
-		
-		SoyFileSet soyFileSet = fileSetBuilder.build();
-		return soyFileSet.compileToJavaObj();
+	private SoyTofu compileTemplates() {
+
+        Resource templatesLocation = closureTemplateConfig.getTemplatesLocation();
+        List<File> templateFiles;
+        try {
+            File baseDirectory = templatesLocation.getFile();
+            if (baseDirectory.isDirectory()) {
+                templateFiles = findSoyFiles(baseDirectory, closureTemplateConfig.isRecursive());
+            } else {
+                throw new IllegalArgumentException("Soy template base directory '" + templatesLocation + "' is not a directory");
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Soy template base directory '" + templatesLocation + "' does not exist", e);
+        }
+
+        SoyFileSet.Builder fileSetBuilder = new SoyFileSet.Builder();
+        for (File templateFile: templateFiles) {
+            fileSetBuilder.add(templateFile);
+        }
+
+        SoyFileSet soyFileSet = fileSetBuilder.build();
+        return soyFileSet.compileToJavaObj();
 	}
 
 	public void setClosureTemplateConfig(ClosureTemplateConfig closureTemplateConfig) {
@@ -65,20 +65,41 @@ public class ClosureTemplateViewResolver extends AbstractTemplateViewResolver {
 	protected Class<?> getViewClass() {
 		return ClosureTemplateView.class;
 	}
-	
+
 	@Override
 	protected AbstractUrlBasedView buildView(String viewName) throws Exception {
 		ClosureTemplateView view = (ClosureTemplateView) super.buildView(viewName);
 		view.setTemplateName(viewName);
-		
+
 		if (isCache()) {
 			view.setCompiledTemplates(compiledTemplates);
 		} else {
 			view.setCompiledTemplates(compileTemplates());
 		}
-		
+
 		return view;
 	}
-	
-	
+
+	protected List<File> findSoyFiles(File baseDirectory, boolean recursive) {
+        List<File> soyFiles = new ArrayList<File>();
+        findSoyFiles(soyFiles, baseDirectory, recursive);
+        return soyFiles;
+    }
+
+	protected void findSoyFiles(List<File> soyFiles, File baseDirectory, boolean recursive) {
+        File[] files = baseDirectory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    if (file.getName().endsWith(".soy")) {
+                        soyFiles.add(file);
+                    }
+                } else if (file.isDirectory() && recursive) {
+                    findSoyFiles(soyFiles, file, recursive);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Unable to retrieve contents of: " + baseDirectory);
+        }
+    }
 }
